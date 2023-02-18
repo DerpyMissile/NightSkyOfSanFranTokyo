0a8920961900bd2e4f1bb00984d33983fa47c98b
DerpyMissile <hmai015@ucr.edu>
2023-02-18 14:58:01 -0800

    more intro stuff

---
+++ Night Sky of San FranTokyo/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public GameObject player;
+    public BoxCollider2D[] walls;
+    public Camera mainCam;
+    public Color blackScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
+        for(int i=0; i<walls.Length; ++i){
+            if(player.GetComponent<BoxCollider2D>().IsTouching(walls[i])){
+                if(i==0){
+                    player.transform.position = walls[1].transform.position;
+                }
+            }
+        }
+    }
+}
