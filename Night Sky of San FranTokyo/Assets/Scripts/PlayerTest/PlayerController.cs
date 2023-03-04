9ce0b0b682b7b473b8b67e9ec166d49f26ecea54
Falak Tulsi <falaktulsi@gmail.com>
2023-03-04 01:02:19 -0800

    Added Base Inventory System
    Able to add items to inventory
    Able to toggle inventory
    Helper functions to manage items in inventory: add, remove, hasAtLeast, etc

---
+++ Night Sky of San FranTokyo/Assets/Scripts/PlayerTest/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private void Update() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+}
