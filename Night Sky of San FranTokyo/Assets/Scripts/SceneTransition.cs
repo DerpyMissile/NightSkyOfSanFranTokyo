cfd218e8193d0d6e0dcab2600327647337930115
DerpyMissile <hmai015@ucr.edu>
2023-03-12 16:24:03 -0700

    scene stuff

--- Night Sky of San FranTokyo/Assets/Scripts/SceneTransition.cs
+++ Night Sky of San FranTokyo/Assets/Scripts/SceneTransition.cs
@@ -6,8 +6,10 @@ public class SceneTransition : MonoBehaviour
 {
     public GameObject player;
     public BoxCollider2D[] walls;
+    public BoxCollider2D[] cameraNoWalls;
     public Camera mainCam;
     public Color blackScreen;
+    bool cameraDetached = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +20,21 @@ public class SceneTransition : MonoBehaviour
     void Update()
     {
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(5, 0);
+        for(int i=0; i<cameraNoWalls.Length; ++i){
+            if(player.GetComponent<BoxCollider2D>().IsTouching(cameraNoWalls[i])){
+                if(cameraDetached){
+                    mainCam.transform.parent = null;
+                }else{
+
+                }
+            }
+        }
         for(int i=0; i<walls.Length; ++i){
             if(player.GetComponent<BoxCollider2D>().IsTouching(walls[i])){
-                if(i==0){
-                    player.transform.position = new Vector2(walls[1].transform.position.x, -26);
+                if(i%2==0){
+                    player.transform.position = new Vector2(walls[i+1].transform.position.x+5, -26);
+                }else{
+                    player.transform.position = new Vector2(walls[i-1].transform.position.x-5, -26);
                 }
             }
         }
