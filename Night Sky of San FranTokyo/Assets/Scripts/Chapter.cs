c6817f67ab4c283d3621fee38ce7976a24deafe5
DerpyMissile <hmai015@ucr.edu>
2023-05-25 00:34:46 -0700

    chapter

---
+++ Night Sky of San FranTokyo/Assets/Scripts/Chapter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Chapter
+{
+    static int chapNum = 0;
+
+    public static void incChap(){
+        chapNum++;
+    }
+}
